a1b45d08130c71eb0710db06925d9fde0a7b9b47
ysiddiqi <ysiddiqi@users.noreply.github.com>
2021-02-28 21:15:23 -0500

    Desktop mode finished

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+  function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
   
  
